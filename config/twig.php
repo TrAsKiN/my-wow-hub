2924e2a02f034c2750122e9f4af8eb15a16d87d0
TrAsKiN <marchal.simon@gmail.com>
2016-08-06 05:38:21 +0200

    Template file reorganization

--- config/twig.php
+++ config/twig.php
@@ -4,11 +4,8 @@ use \Cocur\Slugify\Slugify;
 use \Cocur\Slugify\Bridge\Twig\SlugifyExtension;
 
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
-$twig = new Twig_Environment($loader);
-$twig->addExtension(new SlugifyExtension(Slugify::create()));
-/*
 $twig = new Twig_Environment($loader, array(
     'debug' => true
 ));
+$twig->addExtension(new SlugifyExtension(Slugify::create()));
 $twig->addExtension(new Twig_Extension_Debug());
-*/
