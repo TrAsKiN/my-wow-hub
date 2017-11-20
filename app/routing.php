b2e07d7654db48acb107ab13a113c3bd64fd6014
TrAsKiN <marchal.simon@gmail.com>
2017-11-20 13:51:58 +0100

    Removing url forcing

--- app/routing.php
+++ app/routing.php
@@ -1,21 +1,16 @@
 <?php
 
 $app->get('/', "MyWoWHub\Controller\Index::home")
-    ->requireHttp()
     ->bind('home');
 
 $app->get('/logout', "MyWoWHub\Controller\Index::logout")
-    ->requireHttp()
     ->bind('logout');
 
 $app->get('/callback', "MyWoWHub\Controller\Callback::register")
-    ->requireHttps()
     ->bind('callback');
 
 $app->get('/{realm}/guild/{guild}', "MyWoWHub\Controller\Guild::show")
-    ->requireHttp()
     ->bind('guild');
 
 $app->get('/{realm}/character/{character}', "MyWoWHub\Controller\Character::show")
-    ->requireHttp()
     ->bind('character');
