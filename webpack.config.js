20e3efd1455bb111668792a16bd4fc601120897b
TrAsKiN <marchal.simon@gmail.com>
2020-10-13 21:27:47 +0200

    Change public path according to environment

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,20 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
+if (Encore.isProduction()) {
+    Encore
+        .setPublicPath('/my-wow-hub/build')
+        .setManifestKeyPrefix('build')
+    ;
+} else {
+    Encore.setPublicPath('/build');
+}
+
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    // .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
